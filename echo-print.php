d63be36bc0e54e5a5463eb4f0a915a780e0344fc
liujianli <376122478@qq.com>
2020-05-18 20:16:47 +0800

    echo print 的使用

---
+++ echo-print.php
@@ -0,0 +1,18 @@
+<?php
+
+echo '<h1>learning php</h1>';
+echo '<h1>', 'hello world', '</h1>';
+
+$name = 'liu';
+$hobby = ['ball', 'play', 'run'];
+echo '我的名字是：{$name}';  // 使用单引号，默认将其认为是字符串输出
+echo "<br>";
+echo "我的名字是：$name";
+echo "<br>";
+echo "爱好：$hobby[0]";
+
+print "<h2>我要学习</h2>";
+print "$name"
+
+
+?>
